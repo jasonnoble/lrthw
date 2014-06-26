25245abf87df6e1aabc2446944544703feb5d5cd
Bill Adkins <bill@12locks.com>
2014-06-25 21:18:57 -0600

    Chapter_1

---
+++ Chapter_1/ex1.rb
@@ -0,0 +1,9 @@
+puts 'Hello World!'
+puts 'Hello again'
+puts 'I like typing this.'
+puts 'this is fun.'
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts 'this is just to see what happens
+'
