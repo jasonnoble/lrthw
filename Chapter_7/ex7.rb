256a511f946a983ce71f1058f890cc0c9922a19d
Bill Adkins <bill@12locks.com>
2014-06-26 18:49:12 -0600

    Chapter_7

---
+++ Chapter_7/ex7.rb
@@ -0,0 +1,35 @@
+puts "mary had a little lamb"
+# prints the string of text "mary had..."
+puts "its fleece was white as %s." % 'snow'
+# prints the string of text "its fleece..." on a separate line
+puts "and everywhere that Mary went."
+# prints the string of text "and everywhere..." on a separate line
+puts "." * 10 # what'd that do?
+# this put a total of ten dots in the line following "and everywhere..."
+end1 = "C"
+# everything with end is a variable that is defined here and printed below.
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here. change it to puts
+# and see what happens
+
+print end1 + end2 + end3 + end4 + end5 + end6
+# these two lines join together (+) all of the defined variables above
+# the "print" command prints on the same line whereas the "puts" command prints
+# everything on a separate line.
+print  end7 + end8 + end9 + end10 + end11 + end12
+
+# this is just polite use of the terminal, try removing it
+
+puts
+# removing this line put the end of the program on the same line as the terminal cursor.
