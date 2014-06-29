2566000277489c3b0699e8b0b3f984636f159287
Bill Adkins <bill@12locks.com>
2014-06-29 16:20:09 -0600

    fixed ex3s

--- Chapter_3/ex3_floating.rb
+++ Chapter_3/ex3_floating.rb
@@ -2,35 +2,35 @@
 puts 'I will now count my chickens:'
 
 # checking that the algebra works...
-puts 'Hens', 25 + 30 / 6
+puts 'Hens', 25.0 + 30 / 6
 
-puts 'Roosters', 100 - 25 * 3 % 4
+puts 'Roosters', 100 - 25.0 * 3 % 4
 
-puts 'now I will count the eggs:'
+puts 'Now I will count the eggs:'
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 # seeing how test operators work
-puts 'is it true that 3+2<5-7?'
+puts 'Is it true that 3 + 2 < 5 - 7?'
 
 # an equation like this will result in a true/false answer
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2 < 5 - 7
 
 # practicing math operators. Spaces between the number and the operator is important
-puts 'what is 3+2?', 3 + 2
+puts 'What is 3 + 2?', 3.0 + 2
 
 # more practice with math
-puts 'what is 5-7?', 5 - 7
+puts 'What is 5 - 7?', 5.0 - 7
 
 # you are answering in a phrase what the operators showed aboove (true/false)
-puts "oh, that's why it's false"
+puts "Oh, that's why it's false"
 
 
-puts 'how about some more?'
+puts 'How about some more.'
 puts
 # this is getting to true/false outputs
 # the answers to the following will be true/false based on how the equation was set up
 # the printout is asking the question that will be answered by the operation.
-puts 'is it greater?', 5 > - 2
-puts 'is it greater or equal?', 5 >= - 2
-puts 'is it less or equal?', 5 <= - 2
+puts 'Is it greater?', 5.0 > - 2
+puts 'Is it greater or equal?', 5.0 >= - 2
+puts 'Is it less or equal?', 5.0 <= - 2
